65ac00ea1c4a86ba229b9af58530f2588e47682e
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-02-26 20:11:03 -0700

    done

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,14 +3,20 @@ import React from "react";
 import Container from "../Container";
 import Logo from "./Logo";
 import UserMenu from "./UserMenu";
+import { User } from "@prisma/client";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar = ({ currentUser }: NavbarProps) => {
   return (
     <nav className="fixed w-full bg-white z-10">
       <div className="py-4 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
+            {currentUser?.name}
             <UserMenu />
           </div>
         </Container>
